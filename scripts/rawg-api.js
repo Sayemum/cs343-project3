0cefb105262f01d3a9f9c2bbbf43160872398f0a
Sayemum Hassan <sayemumhassan@gmail.com>
2023-05-02 15:17:32 -0400

    fixed apiKey for rawg api

--- scripts/rawg-api.js
+++ scripts/rawg-api.js
@@ -1,4 +1,4 @@
-const apiKey = "5edb6047f7msh3b456d5178c2fefp1e7d64jsn00f08b160820"; // Replace with your actual API key
+const apiKey = "c5ffe1f1b9cf4c75b41365e7f3ad5278"; // Replace with your actual API key
 
 async function searchVideoGames(query) {
   const url = new URL("https://rawg-video-games-database.p.rapidapi.com/games");
